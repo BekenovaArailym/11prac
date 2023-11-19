5fe9933b6e2bfcf88c5ac17f64c91f446dcbaf38
a_paradise <124023095+BekenovaArailym@users.noreply.github.com>
2023-11-19 22:41:17 +0600

    Program.cs

--- Mod11prac/Program.cs
+++ Mod11prac/Program.cs
@@ -13,15 +13,15 @@ namespace Mod11prac
         {
             Employee employee1;
 
-            employee1.name = "біреу";
+            employee1.name = "Айрис";
             employee1.vacancy = Vacancies.PoliceOfficer;
-            employee1.hireDate = new int[] { 2022, 5, 15 };
-            employee1.salary = 250000;
+            employee1.hireDate = new int[] { 2020, 9, 01 };
+            employee1.salary = 1900000;
 
-            Console.WriteLine($"Аты: {employee1.name}");
-            Console.WriteLine($"Қызметі: {employee1.vacancy}");
-            Console.WriteLine($"Жұмысқа қабылданған уақыты: {employee1.hireDate[0]}.{employee1.hireDate[1]}.{employee1.hireDate[2]}");
-            Console.WriteLine($"Жалақысы: {employee1.salary}");
+            Console.WriteLine($"Имя: {employee1.name}");
+            Console.WriteLine($"Должность: {employee1.vacancy}");
+            Console.WriteLine($"Время когда его приняли на работу: {employee1.hireDate[0]}.{employee1.hireDate[1]}.{employee1.hireDate[2]}");
+            Console.WriteLine($"ЗП: {employee1.salary}");
 
             Console.ReadLine();
         }
